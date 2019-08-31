42541f9be100c04065a88dea3e5a3b473dd8bac3
MrOtherGuy <jastekken@outlook.com>
2019-08-31 10:59:47 +0300

    Handle infinite & near-infinite video duration for twitch

--- frame_script.js
+++ frame_script.js
@@ -7,12 +7,44 @@
   window.faviconFrame = true;
   
   const isTopFrame = window.top === window;
-
-  function getProgress(a){
-    if(!a.duration){
+  
+  function getTwitchDuration(video){
+    let el = document.querySelector(".js-player-slider");
+    if(!el){
+      return -1
+    }
+    let duration = el.getAttribute("aria-valuemax");
+    if(!duration){
+      return -1
+    }    
+    return Number(duration)
+  }
+  
+  function getDuration(video){
+    let mDuration = video.duration;
+    if(mDuration > 36000){
+      if(video.overrideDuration){
+        mDuration = video.overrideDuration;
+      }else{
+        let maybe = getTwitchDuration(video);
+        if( maybe > 0 ){
+          mDuration = maybe;
+          
+          video.overrideDuration = maybe;
+          video.addEventListener("durationchange",(e)=>(e.target.overrideDuration=null),{once:true});
+        }
+      }
+    }
+    return mDuration
+  }
+  
+  function getProgress(video){
+    if(!video.duration || video.duration === Infinity){
       return 0
     }
-    return a.currentTime/a.duration
+    let mDuration = getDuration(video);
+    
+    return video.currentTime / mDuration
   }
 
   function getVideo(){
@@ -58,7 +90,7 @@
     if(!video || video.observed){
       return 1
     }else{
-      video.addEventListener("play",function(){ window.faviconFrame = true; intervalFn = setInterval(edit,2000) });
+      video.addEventListener("play",function(){ window.faviconFrame = true; edit(); intervalFn = setInterval(edit,getIntervalLength(getDuration(video))) });
       video.addEventListener("pause",clearInter);
       video.addEventListener("ended",clearInter);
       video.addEventListener("abort",clearInter);
@@ -67,10 +99,16 @@
     return 0
   }
   
+  function getIntervalLength(duration){
+    if( duration < 60 ){ return 1000 }
+    else if( duration < 240 ){ return 2000 }
+    else{ return 5000 }
+  }
+  
   let intervalFn = null;
 
   if(!init()){
-    intervalFn = setInterval(edit,2000);
+    intervalFn = setInterval(edit,getIntervalLength(getDuration(getVideo())));
   }
 
 })();
